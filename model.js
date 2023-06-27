2332b6d0b48552bf944cbceb77d7969e261c87b3
Gary-Huynh <gary.c.huynh@gmail.com>
2023-06-27 14:44:21 +0100

    added deleting comments ticket 9

--- model.js
+++ model.js
@@ -79,3 +79,13 @@ exports.createArticleComment = (article_id,username,commentBody)=>{
        return newComment.rows
     })
 }
+
+exports.deleteSelectedComment = (comment_id)=>{
+    return db.query("DELETE FROM comments WHERE comment_id = $1 RETURNING *;",[comment_id])
+    .then((result)=>{
+        if(!result.rows[0]){
+            return Promise.reject({status:404, msg:"Not Found"})
+        }
+        return result.rows[0]
+    })
+}
