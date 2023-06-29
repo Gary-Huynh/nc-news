b30342dfb99c651f6bf26f456b250bab59ff8160
Gary-Huynh <gary.c.huynh@gmail.com>
2023-06-29 16:17:07 +0100

    seperated app js into several routers

--- controller.js
+++ controller.js
@@ -19,10 +19,6 @@ exports.getAllTopics = (req, res, next)=>{
     })
     .catch(next)
 }
-exports.getAllEndpoints = (req, res, next)=>{
- 
-        res.status(200).send(endpoints)
-}
 
 exports.getSpecificArticle = (req,res,next)=>{
     selectSpecificArticle(req.params.article_id)
